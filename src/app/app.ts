4f233c707da9a3926ec4b01a36f88d1903ec54c7
לוי ריקי <1607010@main.local>
2025-11-02 16:18:54 +0200

    lesson4

--- src/app/app.ts
+++ src/app/app.ts
@@ -4,20 +4,27 @@ import { Header } from './header/header';
 import { User } from './user/user';
 import { USERS } from './fake_users';
 
-
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet , Header,User],
+  imports: [RouterOutlet, Header, User],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
 
-
 export class App {
-
-  protected readonly title = signal('my-first-app');
   users = USERS;
+  protected readonly title = signal('lesson-3');
+
+  selectedUser = this.users[0];
+
+  onUserSelected(idUserClicked: string){
+    const newUser =  this.users.find((user)=>user.id == idUserClicked);
+    if(newUser)
+      this.selectedUser = newUser
+  }
 }
+
+
 
 
 
--- src/app/user/user.ts
+++ src/app/user/user.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component , Input , Output , EventEmitter} from '@angular/core';
 import { USERS} from '../fake_users';
 
 
@@ -11,73 +11,30 @@ import { USERS} from '../fake_users';
 
 export class User {
 
-  selectedUser= USERS[0];
+  // selectedUser= USERS[0];
   // @Input({ required: true }) user!: UserData;
-  @Input() name!: string;
-  @Input() avatar!: string;
+  @Input() name: string ='';
+  @Input() avatar: string = '';
+  @Input({required: true}) id!: string;
   
+  @Output() userclicked = new EventEmitter<string>();
+
   get  userImgPath(){
     return 'assets/users/' + this.avatar;
   }
-  changeUser(){
-    const randomIndex = Math.floor(Math.random() * USERS.length);
-    this.selectedUser =USERS[randomIndex];
-  }
 
+  onUserClicked(){
+
+    this.userclicked.emit(this.id)
+  }
+  
+
+
+  // onUserSelected(id: string){}
   
 
 }
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// import { Component, Input } from '@angular/core';
-// import {UserData} from '../fake_users';
-
-
-// @Component({
-//   selector: 'app-user',
-//   imports: [],
-//   templateUrl: './user.html',
-//   styleUrl: './user.css'
-// })
-
-// export class User {
-
-//   @Input({ required: true }) user!: UserData;
-
-  
-//   get  userImgPath(){
-//     return 'assets/users/' + this.user.avatar;
-//   }
-
-  
-
-// }
 
